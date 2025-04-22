7f82c895b058fcfaee492651cfe2b12c8d624c65
Junaid Ahmed <junaidahmed9038@gmail.com>
2025-04-22 23:29:17 +0530

    New page done

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/Layout/Hero'
+import GenderCollection from '../components/Products/GenderCollection'
+import NewArrivals from '../components/Products/NewArrivals'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <GenderCollection/>
+      <NewArrivals/>
+      {/* Best Seller */}
+    </div>
+  )
+}
+
+export default Home
